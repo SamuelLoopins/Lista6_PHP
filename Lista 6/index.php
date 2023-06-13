2f4b37d986d80e405c995cd731be87eae3e2c79f
SamuelLoopins <samugomes231@gmail.com>
2023-06-13 08:09:59 -0300

    asd

--- Lista 6/index.php
+++ Lista 6/index.php
@@ -4,10 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <h3>Página inicial - Agenda IFSP</h3>
+    <div class="alert alert-success" role="alert">
+        <h3>Página inicial - Agenda IFSP</h3>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">  
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home<span class="sr-only">(Página atual)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="Cadastro_agenda.html">Cadastrar Agenda</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="Listar_agenda.php">Listar Agendas</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled" href="#">Desativado</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
         <li><a href="Cadastro_agenda.html">Cadastrar Agenda</a></li>
         <li><a href="Listar_agenda.php">Listar Agenda</a></li>
     </ul>
